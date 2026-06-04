928cc58996589fc0adf8015736357d377eeaaec8
Matheus Lopes <mmirandalopes97@gmail.com>
2026-06-04 20:35:59 -0300

    Commit Sistemas de Votos

---
+++ main_sistema.py
@@ -0,0 +1,35 @@
+linguagens = {}
+
+for i in range (0,3):
+    nome = input("Informe o seu nome: ")
+
+    print("Informe a linguagem de programação dentre as opções (1) C++ - (2) Java - (3) Python")
+
+    op = int(input("Digite a sua opção (1, 2 e 3): "))
+
+    if op == 1:
+        linguagens[nome] = 'C++'
+    elif op == 2:
+        linguagens[nome] = 'Java'
+    elif op == 3:
+        linguagens[nome] = 'Python'
+
+print(linguagens)
+
+c = 0 
+java = 0
+python = 0
+
+for valor in linguagens.values():
+    if valor == 'C++':
+        c += 1
+    elif valor == 'Java':
+        java += 1
+    elif valor == 'Python':
+        python += 1
+
+print("\n RESULTADO")
+
+print('C++: ' + str(c))
+print('Java: ' + str(java))
+print('Python: ' + str(python))
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Sistema de Votação
+
+## Esse é um sistema de votação, onde é utilizado uma lsita vazia, e em seguida preenchido e somado após os inputs.
+
+### Assinado: Matheus Lopes
